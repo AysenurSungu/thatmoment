890e80fd6942f12e45a60df85741bdb5765cb727
aysenur-sungu <sungu.aysenur@gmail.com>
2026-01-12 00:20:42 +0300

    feat(profile): add journal lock with password flow

--- src/main/java/com/thatmoment/modules/profile/api/UserProfileController.java
+++ src/main/java/com/thatmoment/modules/profile/api/UserProfileController.java
@@ -19,6 +19,7 @@ import jakarta.validation.Valid;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
